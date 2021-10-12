1c9221dfb5273cd4a023edac079dc575313d607f
Siddhant <me@madebysid.com>
2021-10-12 21:10:02 +0200

    fix: do not swallow errors from seed scripts (#53)

--- nextJS/prisma/seed.ts
+++ nextJS/prisma/seed.ts
@@ -43,10 +43,6 @@ async function main() {
   )
 }
 
-main()
-  .catch((e) => {
-    console.error(e)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+main().finally(async () => {
+  await prisma.$disconnect()
+})
--- rentalsPlatform/prisma/seed.ts
+++ rentalsPlatform/prisma/seed.ts
@@ -115,10 +115,6 @@ async function main() {
   }
 }
 
-main()
-  .catch((e) => {
-    console.error(e)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+main().finally(async () => {
+  await prisma.$disconnect()
+})
